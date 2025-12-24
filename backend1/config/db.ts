c7a296964af8cfff621f737e9b7ba22a12a3e591
Helal Uddin -SWE <seoguro1999@gmail.com>
2025-12-25 03:23:38 +0600

    update for prodict

--- backend1/config/db.ts
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose")
-const connectDB=async()=>{ 
-   await  mongoose.connect(process.env.DATABASE_URL),{ useNewUrlParser: true,
-      useUnifiedTopology: true,}
-      console.log("mongo connected");
-      
-}
-module.exports=connectDB
